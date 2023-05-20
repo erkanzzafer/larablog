41d059d6a2436b58c13a95327450f666170e7968
hasanboke <120455548+hasanboke@users.noreply.github.com>
2023-05-20 13:49:35 +0300

    author middleware

---
+++ resources/views/back/pages/auth/login.blade.php
@@ -0,0 +1,22 @@
+@extends('back.layouts.auth-layout')
+@section('pageTitle',isset($pageTitle) ? $pageTitle : 'login')
+
+@section('content')
+<div class="page page-center">
+    <div class="container container-tight py-4">
+      <div class="text-center mb-4">
+        <a href="." class="navbar-brand navbar-brand-autodark"><img src="./static/logo.svg" height="36" alt=""></a>
+      </div>
+      <div class="card card-md">
+        <div class="card-body">
+          <h2 class="h2 text-center mb-4">Login to your account</h2>
+            @livewire('author-login-form')
+        </div>
+        </div>
+      </div>
+      <div class="text-center text-muted mt-3">
+        Don't have account yet? <a href="./sign-up.html" tabindex="-1">Sign up</a>
+      </div>
+    </div>
+  </div>
+@endsection
